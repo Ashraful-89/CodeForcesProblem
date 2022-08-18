f5abd0292183e4be7034c4bd3f71820e40b3f540
Ashraful Islam <ashrafulcse.ru@gmail.com>
2022-08-19 01:51:55 +0600

    Burenka Plays with Fractions

---
+++ 1720A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(void){
+    int t; cin >> t;
+    while(t--){
+        int a , b , c, d;
+        cin >> a >> b >> c >> d;
+        if(((double) a / (double) b )== ((double) c / (double) d)) cout << '0' << endl;
+        else if( a == 0 || c == 0) cout << "1" << endl;
+        else if( (a * c) % (b * d) == 0 || (c * b) % (a * d) == 0) cout << "1" << endl;
+        else cout <<"2" << endl;
+
+    }
+    return 0;
+}
